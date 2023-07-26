ba7058532b880f1c2698323231d68065cd5721ad
David Mickael <raks.david1@gmail.com>
2023-07-26 17:02:04 +0300

    menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,15 @@
-$ = require('jquery')
+import $ from 'jquery'
 
-$(function() {
-    console.log('charged');
+import montserrat from '@fontsource/montserrat'
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import '../sass/app.sass'
+
+$(function() { 
+    $('.avatar').addClass('rounded-circle')
+
+    $('.nav-menu li').on('click', function(ev) {
+        $('.nav-menu li').removeClass('active')
+        $(this).addClass('active')
+    })
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development',
     resolve: {
         extensions: ['.js', '.jsx']
     },
+    watch: true,
     entry: './assets/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
 };
